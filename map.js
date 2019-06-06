e6906c4a18529356846ed37c2037518be54edfc6
vertsol <50719863+vertsol@users.noreply.github.com>
2019-06-06 16:37:09 -0700

    Add files via upload

--- map.js
+++ map.js
@@ -4,7 +4,6 @@ var map = L.map('map', {
 
 })
 
-
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
